bf56126a2eac3834d808be96ef298d0ea5a00725
Raul Sousa Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2026-05-03 10:13:18 -0300

    config: installed all jest dependencies and scrips to run tests within the frontend

---
+++ packages/pitang-frontend/jest.config.ts
@@ -0,0 +1,25 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+          module: 'commonjs',
+          verbatimModuleSyntax: false,
+          esModuleInterop: true,
+          paths: {
+            '@/*': ['./src/*'],
+          },
+        },
+      },
+    ],
+  },
+};
