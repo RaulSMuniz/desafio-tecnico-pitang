17996892e203a41de354de5419d9fe0d52ee40fd
Raul Sousa Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2026-05-03 18:32:50 -0300

    fix: fixed http_port default being 3131, now it's 3333

--- packages/pitang-backend/src/core/EnvVars.ts
+++ packages/pitang-backend/src/core/EnvVars.ts
@@ -6,7 +6,7 @@ const environmentSchema = z.object({
         .string()
         .startsWith("postgresql://", "A URL deve começar com postgresql://"),
 
-    HTTP_PORT: z.coerce.number().default(3131),
+    HTTP_PORT: z.coerce.number().default(3333),
     JWT_SECRET: z.string().min(8, "A SECRET deve ter pelo menos 8 caracteres"),
     NODE_ENV: z.string().default("development"),
     BACKEND_URL: z.string(),
