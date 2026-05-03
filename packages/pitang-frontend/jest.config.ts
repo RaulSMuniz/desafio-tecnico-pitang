b869b41f4cef29fecd90d45e4040e7b8000beb21
Raul Sousa Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2026-05-03 14:16:03 -0300

    refactor: changed jest.config.ts to understand PNG, etc... and created fileMock.js within __mocks__ so that the tests can understand the pitang-logo.png file.

--- packages/pitang-frontend/jest.config.ts
+++ packages/pitang-frontend/jest.config.ts
@@ -3,8 +3,9 @@ export default {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.tsx?$': [
