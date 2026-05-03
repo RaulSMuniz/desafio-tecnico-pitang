0dd4097b3952c2e5450b43693f33679485e80d00
Raul Sousa Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2026-05-03 16:20:14 -0300

    fix: fixed all schema inconsistencies such as userSchema being 8 characters, and loginSchema being 6 characters. Fixed reimbursementSchema so that it doesn't allow future dates on the backend. Fixed historySchema not having all 'acao' enums, even if they're not used.

--- packages/pitang-backend/src/schemas/index.ts
+++ packages/pitang-backend/src/schemas/index.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const userSchema = z.object({
     nome: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
     email: z.email("O email deve ser válido"),
-    senha: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
+    senha: z.string().min(8, "A senha deve ter pelo menos 8 caracteres"),
     perfil: z.enum(["COLABORADOR", "GESTOR", "FINANCEIRO", "ADMIN"]).optional(),
 });
 
@@ -16,7 +16,7 @@ export const updateUserSchema = z.object({
 
 export const loginSchema = z.object({
     email: z.email("O email deve ser válido"),
-    senha: z.string().min(1, "A senha deve ter pelo menos 6 caracteres"),
+    senha: z.string().min(8, "A senha deve ter pelo menos 8 caracteres"),
 });
 
 export const categorySchema = z.object({
@@ -27,7 +27,7 @@ export const categorySchema = z.object({
 export const reimbursementSchema = z.object({
     descricao: z.string().min(3, "A descrição deve ter pelo menos 3 caracteres"),
     valor: z.number().positive("O valor deve ser positivo").min(1, "O valor deve ser maior que zero."),
-    dataDespesa: z.coerce.date(),
+    dataDespesa: z.coerce.date().refine((date) => date <= new Date(), "A data da despesa não pode ser futura."),
     categoriaId: z.number().positive("A categoria deve ser válida"),
     anexos: z.array(z.string()).optional(),
 })
@@ -38,8 +38,8 @@ export const rejectionSchema = z.object({
 
 
 export const attachmentSchema = z.object({
-    nomeArquivo: z.string(),
-    tipoArquivo: z.string(),
+    nomeArquivo: z.string().min(1, "O nome do arquivo é obrigatório"),
+    tipoArquivo: z.string().regex(/^(image\/(jpeg|png|jpg)|application\/pdf)$/, "Formato de arquivo não permitido. Use apenas Imagens ou PDF."),
 })
 
 export const paramId = z.object({
@@ -54,12 +54,6 @@ export const historySchema = z.object({
     solicitacaoId: z.uuid("ID de reembolso inválido"),
     observacao: z.string(),
     criadoEm: z.coerce.date(),
-    acao: z.enum(["ENVIADO", "APROVADO", "REJEITADO"]),
+    acao: z.enum(["RASCUNHO", "ENVIADO", "APROVADO", "PAGO", "REJEITADO", "CANCELADO"]),
     usuarioId: z.uuid("ID de usuário inválido"),
 })
-
-export const paginationQuery = z.object({
-    page: z.coerce.number().default(1),
-    pageSize: z.coerce.number().max(100).default(20),
-    sort: z.enum(["asc", "desc"]).default("asc"),
-});
